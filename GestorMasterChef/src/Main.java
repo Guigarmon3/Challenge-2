2aa57974a8db2cfbcd627e1a83901f89b36a224b
Guigarmon3 <guigarmon3@alu.edu.gva.es>
2026-05-18 13:18:28 +0200

    Final

--- GestorMasterChef/src/Main.java
+++ GestorMasterChef/src/Main.java
@@ -1,20 +1,33 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("""
---- MENÚ MASTERCHEF ---
-1. Cargar ficheros de participantes
-2. Mostrar todos los participantes
-3. Buscar participante por nombre
-4. Generar fichero participantes.txt
-5. Mostrar estadísticas
-Escribe 'FIN' para salir
-                """);
+    public static int vueltas = 0;
+    public static void main(String[] args) throws InterruptedException {
 
-        try (BufferedReader respuesta = new BufferedReader(new InputStreamReader(System.in))) {
+        menu();
+    }
+
+    private static void menu() throws InterruptedException {
+
+        if (vueltas != 0) {
+            TimeUnit.SECONDS.sleep(3);
+        } else {vueltas=1;}
+
+        try (BufferedReader respuesta = new BufferedReader(new InputStreamReader(System.in)))  {
+            System.out.println("""
+╔══════════════════════════════════════════╗
+║         --- MENÚ MASTERCHEF ---          ║
+║   1. Cargar ficheros de participantes    ║
+║   2. Mostrar todos los participantes     ║
+║   3.  Buscar participante por nombre     ║
+║   4. Generar fichero participantes.txt   ║
+║   5.       Mostrar estadísticas          ║
+╚══════════════════════════════════════════╝
+         Escribe 'FIN' para salir
+                """);
             System.out.print("tu respuesta es: ");
             String numero = respuesta.readLine();
 
@@ -29,13 +42,17 @@ Escribe 'FIN' para salir
                 case "3" -> Ejercicio3();
                 case "4" -> Ejercicio4();
                 case "5" -> Ejercicio5();
+                case "FIN" -> {
+                    return;
+                }
+                default -> menu();
             }
         } catch (IOException e) {
             System.out.println(e);
         }
     }
 
-    private static void Ejercicio5() {
+    private static void Ejercicio5() throws InterruptedException {
         int contador_rojo = 0;
         int contador_azul = 0;
 
@@ -59,9 +76,10 @@ Escribe 'FIN' para salir
         System.out.println("Participantes en Cocina roja: " + contador_rojo);
         System.out.println("Participantes en Cocina azul: " + contador_azul);
         System.out.println("Total de participantes:  " + (contador_azul + contador_rojo));
+        menu();
     }
 
-    private static void Ejercicio4() {
+    private static void Ejercicio4() throws InterruptedException {
         ArrayList<String> personas = new ArrayList<>();
         String linea;
 
@@ -92,39 +110,41 @@ Escribe 'FIN' para salir
         } catch (IOException e) {
             System.out.println("Error al crear el archivo participantes.txt");
         }
+        menu();
     }
 
-    private static void Ejercicio3() {
-        try (BufferedReader nombreScanner = new BufferedReader(new InputStreamReader(System.in))) {
-            System.out.print("Nombre de la persona: ");
-            String nom = nombreScanner.readLine();
-            String linea;
+    private static void Ejercicio3() throws IOException, InterruptedException {
+        // Perdon claudia, pero aqui no pongo try catch por que no entiendo xq se estan colisionando los 2 Readers
+        // He probado a cerrar el otro y mil cosas, le he preguntado a gemini por que, no la solucion
+        // y este tampoco me daba una solucion clara
+        BufferedReader nombreScanner = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Nombre de la persona: ");
+        String nom = nombreScanner.readLine();
+        String linea;
 
-            try (BufferedReader rojo = new BufferedReader(new FileReader("cocina_roja.txt"))) {
-                while ((linea = rojo.readLine()) != null) {
-                    if (linea.equalsIgnoreCase(nom)) {
-                        System.out.println(linea + " Está en Rojo");
-                    }
+        try (BufferedReader rojo = new BufferedReader(new FileReader("cocina_roja.txt"))) {
+            while ((linea = rojo.readLine()) != null) {
+                if (linea.equalsIgnoreCase(nom)) {
+                    System.out.println(linea + " Está en Rojo");
                 }
-            } catch (IOException e) {
-                System.out.println("No se ha podido leer el archivo cocina_roja.txt");
-            }
-
-            try (BufferedReader azul = new BufferedReader(new FileReader("cocina_azul.txt"))) {
-                while ((linea = azul.readLine()) != null) {
-                    if (linea.equalsIgnoreCase(nom)) {
-                        System.out.println(linea + " Está en Azul");
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("No se ha podido leer el archivo cocina_azul.txt");
             }
         } catch (IOException e) {
-            System.out.println(e);
+            System.out.println("No se ha podido leer el archivo cocina_roja.txt");
         }
+
+        try (BufferedReader azul = new BufferedReader(new FileReader("cocina_azul.txt"))) {
+            while ((linea = azul.readLine()) != null) {
+                if (linea.equalsIgnoreCase(nom)) {
+                    System.out.println(linea + " Está en Azul");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("No se ha podido leer el archivo cocina_azul.txt");
+        }
+        menu();
     }
 
-    private static void Ejercicio2() {
+    private static void Ejercicio2() throws InterruptedException {
         String linea;
 
         try (BufferedReader rojo = new BufferedReader(new FileReader("cocina_roja.txt"))) {
@@ -142,9 +162,10 @@ Escribe 'FIN' para salir
         } catch (IOException e) {
             System.out.println("No se ha podido leer el archivo cocina_azul.txt");
         }
+        menu();
     }
 
-    private static void Ejercicio1() {
+    private static void Ejercicio1() throws InterruptedException {
         ArrayList<String> cantidad = new ArrayList<>();
         String linea;
 
@@ -167,5 +188,6 @@ Escribe 'FIN' para salir
         } catch (IOException e) {
             System.out.println("No se ha podido leer el archivo cocina_roja.txt");
         }
+        menu();
     }
 }
